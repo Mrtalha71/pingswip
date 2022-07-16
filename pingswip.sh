ccaddc62fa48d89c989d08d8edbda3a83553883b
Md. Abu Talha <101306544+Mrtalha71@users.noreply.github.com>
2022-07-16 11:17:05 +0600

    pingswip.sh

---
+++ pingswip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "##### Happy Hacking With  Abu Talha From Bangladesh! Thank you. ###
+               ***************************
+                    ******************
+                        ###########
+                           ^^^^^
+                             @@
+                              ~ "
+#very simple ip finder.
+
+echo "Enter Your Subnet"
+
+read SUBNET
+
+echo "start go ahead "
+
+echo "**** MADE BY TALHA ****"
+
+for IP in $(seq 100 254); do
+        ping -c 1 $SUBNET.$IP
+done
